2dc2c0e85784cfa27f978f459c7756b7f0c5db57
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-02-27 13:56:22 +0530

    basic layout with 4 columns

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,29 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
 	return (
-		<button className='p-2 text-2xl text-green-500 font-bold  border-2 border-b-[5px] border-green-500 rounded-md  hover:cursor-pointer hover:bg-green-500 hover:text-white'>
-			Hello World
-		</button>
+		<>
+			<div className='columns-4'>
+				<div className='w-full container mx-auto'>
+					<button className='m-1.5 p-2 text-2xl text-green-500 font-bold  border-2 border-b-[5px] border-green-500 rounded-md  hover:cursor-pointer hover:bg-green-500 hover:text-white'>
+						Hello World
+					</button>
+				</div>
+				<div className='w-full container mx-auto'>
+					<button className='m-1.5 p-2 text-2xl text-green-500 font-bold  border-2 border-b-[5px] border-green-500 rounded-md  hover:cursor-pointer hover:bg-green-500 hover:text-white'>
+						Hello World
+					</button>
+				</div>
+				<div className='w-full container mx-auto'>
+					<button className='m-1.5 p-2 text-2xl text-green-500 font-bold  border-2 border-b-[5px] border-green-500 rounded-md  hover:cursor-pointer hover:bg-green-500 hover:text-white'>
+						Hello World
+					</button>
+				</div>
+				<div className='w-full container mx-auto'>
+					<button className='m-1.5 p-2 text-2xl text-green-500 font-bold  border-2 border-b-[5px] border-green-500 rounded-md  hover:cursor-pointer hover:bg-green-500 hover:text-white'>
+						Hello World
+					</button>
+				</div>
+			</div>
+		</>
 	);
 }
