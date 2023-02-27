5963ceb6e12d54828ff0fb59f722fab6a7c1423d
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-02-27 14:02:41 +0530

    extract button as a component

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,21 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import styles from './page.module.css';
-
-const inter = Inter({ subsets: ['latin'] });
+function Button() {
+	return (
+		<div className='container mx-auto'>
+			<button className='m-1.5 p-2 text-xl text-green-500 font-bold  border-2 border-b-[5px] border-green-500 rounded-md  hover:cursor-pointer hover:bg-green-500 hover:text-white'>
+				Hello World
+			</button>
+		</div>
+	);
+}
 
 export default function Home() {
 	return (
 		<>
 			<div className='columns-4'>
-				<div className='w-full container mx-auto'>
-					<button className='m-1.5 p-2 text-2xl text-green-500 font-bold  border-2 border-b-[5px] border-green-500 rounded-md  hover:cursor-pointer hover:bg-green-500 hover:text-white'>
-						Hello World
-					</button>
-				</div>
-				<div className='w-full container mx-auto'>
-					<button className='m-1.5 p-2 text-2xl text-green-500 font-bold  border-2 border-b-[5px] border-green-500 rounded-md  hover:cursor-pointer hover:bg-green-500 hover:text-white'>
-						Hello World
-					</button>
-				</div>
-				<div className='w-full container mx-auto'>
-					<button className='m-1.5 p-2 text-2xl text-green-500 font-bold  border-2 border-b-[5px] border-green-500 rounded-md  hover:cursor-pointer hover:bg-green-500 hover:text-white'>
-						Hello World
-					</button>
-				</div>
-				<div className='w-full container mx-auto'>
-					<button className='m-1.5 p-2 text-2xl text-green-500 font-bold  border-2 border-b-[5px] border-green-500 rounded-md  hover:cursor-pointer hover:bg-green-500 hover:text-white'>
-						Hello World
-					</button>
-				</div>
+				<Button />
+				<Button />
+				<Button />
+				<Button />
 			</div>
 		</>
 	);
