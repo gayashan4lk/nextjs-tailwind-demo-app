6df0d2fbefcebb891340567fccf3dd30e3823641
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-02-27 17:04:40 +0530

    design beautiful boxes

---
+++ app/spacing-sizing/page.tsx
@@ -0,0 +1,18 @@
+const SpacingPage = () => {
+	return (
+		<>
+			<div
+				id='big-squar'
+				className='w-8/12 mx-auto h-32 bg-sky-400 opacity-20 rounded-2xl'
+			></div>
+			<div
+				id='small-sqaure'
+				className='w-6/12 h-36 mx-auto -mt-8 bg-sky-300 opacity-95 rounded-2xl align-middle flex items-center justify-center'
+			>
+				<p className='font-bold text-white text-3xl'>-mt-8</p>
+			</div>
+		</>
+	);
+};
+
+export default SpacingPage;
